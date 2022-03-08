82d9b1e1a92db9afb65b64f1e24a1b8138fcfec3
horvatht9411 <horvatht9411@gmail.com>
2022-03-08 11:18:27 +0100

    Add display

--- engine.py
+++ engine.py
@@ -1,3 +1,6 @@
+from turtle import width
+
+
 def create_board(width, height):
     '''
     Creates a new game board based on input parameters.
@@ -9,8 +12,18 @@ def create_board(width, height):
     Returns:
     list: Game board
     '''
-    pass
-
+    board = []
+    for i in range(height):
+        board.append([])
+        for j in range(width):
+            if i == 0 or i == height - 1:
+                board[i].append("▩")
+            else:
+                board[i].append(" ")
+    for left_right in board:
+        left_right[0] = "▩"
+        left_right[-1] = "▩"
+    return board
 
 def put_player_on_board(board, player):
     '''
